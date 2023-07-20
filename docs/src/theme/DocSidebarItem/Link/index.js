b2101b2c5573a662098abd04575eb88f7142978f
rikinsk <rikin@hasura.io>
2023-07-20 16:41:10 +0530

    Revert "hotfix: sidebar link"
    This reverts commit 23ba6ac97f2f57f30685a40bf78f3df1881ff380.

--- docs/src/theme/DocSidebarItem/Link/index.js
+++ docs/src/theme/DocSidebarItem/Link/index.js
@@ -12,84 +12,102 @@ import EnterpriseDark from '@site/static/icons/enterprise-light.svg';
 import CloudLight from '@site/static/icons/cloud-dark.svg';
 import CloudDark from '@site/static/icons/cloud-light.svg';
 import BetaTag from '@site/src/components/BetaTag/BetaTag';
-
 export default function DocSidebarItemLink({ item, onItemClick, activePath, level, index, ...props }) {
   const { href, label, className, autoAddBaseUrl } = item;
   const isActive = isActiveSidebarItem(item, activePath);
   const isInternalLink = isInternalUrl(href);
   const { colorMode } = useColorMode();
 
-  const isDarkMode = colorMode === 'dark';
-
-  const enterpriseIcon = isDarkMode ? <EnterpriseDark/> : <EnterpriseLight/>;
-  const cloudIcon = isDarkMode ? <CloudDark/> : <CloudLight/>;
-  const betaIcon = <BetaTag/>;
-
   // Conditional rendering for sidebar icons
-  let icons;
-  switch (className) {
-    case 'enterprise-icon':
-      icons = enterpriseIcon;
-      break;
-    case 'cloud-icon':
-      icons = cloudIcon;
-      break;
-    case 'enterprise-icon-and-beta':
-      icons = (
-          <>
-            {enterpriseIcon} {betaIcon}
-          </>
-      )
-      break;
-    case 'cloud-and-enterprise-icon':
-      icons = (
-          <>
-            {cloudIcon} {enterpriseIcon}
-          </>
-      )
-      break;
-    case 'beta-icon':
-      icons = betaIcon;
-      break;
+  function addIcons(className) {
+    switch (className) {
+      case 'enterprise-icon':
+        return colorMode === 'dark' ? <EnterpriseDark /> : <EnterpriseLight />;
+      case 'cloud-icon':
+        return colorMode === 'dark' ? <CloudDark /> : <CloudLight />;
+      case 'enterprise-icon-and-beta':
+        return (
+          <div className={styles['sidebar_link_wrapper']}>
+            {colorMode === 'dark' ? (
+              <>
+                <EnterpriseDark /> <BetaTag />
+              </>
+            ) : (
+              <>
+                <EnterpriseLight /> <BetaTag />
+              </>
+            )}
+          </div>
+        );
+      case 'cloud-and-enterprise-icon':
+        return (
+          <div className={styles['cloud-ee-container']}>
+            {colorMode === 'dark' ? (
+              <>
+                <CloudDark /> <EnterpriseDark />{' '}
+              </>
+            ) : (
+              <>
+                <CloudLight /> <EnterpriseLight />
+              </>
+            )}
+          </div>
+        );
+      case 'beta-icon':
+        return (
+          <div className={styles['sidebar_link_wrapper']}>
+            <BetaTag />
+          </div>
+        );
+      default:
+        return null;
+    }
   }
 
-  const labelWithIcons = (
-      <div className={styles['sidebar_link_wrapper']}>
-        {label} {icons}
-      </div>
-  );
-
-  return (
+  if (className != 'sidebar_heading') {
+    return (
       <li
-          className={clsx(
-              ThemeClassNames.docs.docSidebarItemLink,
-              ThemeClassNames.docs.docSidebarItemLinkLevel(level),
-              'menu__list-item',
-              className,
-              styles[`sidebar_link_wrapper`]
-          )}
-          key={label}
+        className={clsx(
+          ThemeClassNames.docs.docSidebarItemLink,
+          ThemeClassNames.docs.docSidebarItemLinkLevel(level),
+          'menu__list-item',
+          className,
+          styles[`sidebar_link_wrapper`]
+        )}
+        key={label}
       >
-        {className !== 'sidebar_heading' ?
-            <Link
-                className={clsx('menu__link', !isInternalLink && styles.menuExternalLink, {
-                  'menu__link--active': isActive,
-                })}
-                autoAddBaseUrl={autoAddBaseUrl}
-                aria-current={isActive ? 'page' : undefined}
-                to={href}
-                {...(isInternalLink && {
-                  onClick: onItemClick ? () => onItemClick(item) : undefined,
-                })}
-                {...props}
-            >
-              {labelWithIcons}
-            </Link>
-            :
-            <>
-              {labelWithIcons}
-            </>
-        }
+        <Link
+          className={clsx('menu__link', !isInternalLink && styles.menuExternalLink, {
+            'menu__link--active': isActive,
+          })}
+          autoAddBaseUrl={autoAddBaseUrl}
+          aria-current={isActive ? 'page' : undefined}
+          to={href}
+          {...(isInternalLink && {
+            onClick: onItemClick ? () => onItemClick(item) : undefined,
+          })}
+          {...props}
+        >
+          {label}
+          {addIcons(className)}
+        </Link>
       </li>
-  );
+    );
+  } else {
+    return (
+      <li
+        className={clsx(
+          ThemeClassNames.docs.docSidebarItemLink,
+          ThemeClassNames.docs.docSidebarItemLinkLevel(level),
+          'menu__list-item',
+          className,
+          styles[`sidebar_link_wrapper`]
+        )}
+        key={label}
+      >
+        {label}
+        {addIcons(className)}
+      </li>
+    );
+  }
 }
